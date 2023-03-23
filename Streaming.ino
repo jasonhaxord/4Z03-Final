b1727099db9e5047c65606259a116588f2df0c48
jasonhaxord <61030109+jasonhaxord@users.noreply.github.com>
2023-03-23 16:14:01 -0400

    Update Streaming.ino

--- Streaming.ino
+++ Streaming.ino
@@ -1,26 +1,14 @@
-/*
-  Using the BNO080 IMU
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: December 21st, 2017
-  SparkFun code, firmware, and software is released under the MIT License.
-	Please see LICENSE.md for further details.
-
-  Feel like supporting our work? Buy a board from SparkFun!
-  https://www.sparkfun.com/products/14586
-
-  This example shows how to output accelerometer values
-
-  Hardware Connections:
-  Attach the Qwiic Shield to your Arduino/Photon/ESP32 or other
-  Plug the sensor onto the shield
-  Serial.print it out at 115200 baud to serial monitor.
-*/
-
 #include <Wire.h>
 
 #include "SparkFun_BNO080_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_BNO080
 BNO080 myIMU;
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char* sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif  // __arm__
+
 
 void setup()
 {
@@ -32,7 +20,7 @@ void setup()
 
   Wire.setClock(400000); //Increase I2C data rate to 400kHz
 
-  myIMU.enableAccelerometer(500); //Send data update every 100ms
+  myIMU.enableAccelerometer(100); //Send data update every 100ms
 }
 
 void loop()
@@ -45,11 +33,12 @@ void loop()
     float z = myIMU.getAccelZ();
 
   
-  float resultant = sqrt(x * x + y * y + z * z);
+  float resultant = abs(x*x);
 
 
-    Serial.print(resultant/9.81, 4);
+    Serial.print(resultant, 4);
     //Serial.print(millis(), 2);
     Serial.println();
   }
+
 }
